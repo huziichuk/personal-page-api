38eac6034ed5352b70b1c7e0003bf22dca97c8fd
guziiuchyk <guziiuchyk@gmail.com>
2025-05-26 15:35:27 +0300

    add .gitignore, add headers, add .htaccess

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo "Hello World!";
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+header('Content-type: json/application');
