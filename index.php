ac0eaf42effddacd33052145e60cecb97b9c6e8d
guziiuchyk <guziiuchyk@gmail.com>
2025-05-26 15:58:51 +0300

    add config, add connect pdo

--- index.php
+++ index.php
@@ -5,3 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 header('Content-type: json/application');
+
+require_once "./utils/config.php";
+require_once "./utils/connect.php";
+
---
+++ utils/config.php
@@ -0,0 +1,11 @@
+<?php
+
+const HOST = "localhost:3307";
+const USER = "root";
+const PASSWORD = "root";
+const DB = "database";
+CONST PORT = 3307;
+const ACCESS_TOKEN_SECRET = 'your_access_token_secret_key';
+const ACCESS_TOKEN_EXPIRATION = 600;
+const REFRESH_TOKEN_SECRET = 'your_refresh_token_secret_key';
+const REFRESH_TOKEN_EXPIRATION = 604800;
